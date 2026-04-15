c5472a0c036da174d9a4ce9884e563898a3893f3
lukmccall <kosmatylukasz@gmail.com>
2026-04-15 12:12:37 +0200

    feat: add kClass property to PType

--- pika-api/src/jvmMain/kotlin/io/github/lukmccall/pika/PType.kt
+++ pika-api/src/jvmMain/kotlin/io/github/lukmccall/pika/PType.kt
@@ -1,8 +1,13 @@
 package io.github.lukmccall.pika
 
+import kotlin.reflect.KClass
+
 /**
  * Represents a runtime type with its Java Class reference.
  *
  * @property jClass Runtime Java class reference. Call `jClass.kotlin` if you need the Kotlin KClass.
  */
-public class PType(public val jClass: Class<*>)
+public class PType(public val jClass: Class<*>) {
+  public val kClass: KClass<*>
+    get() = jClass.kotlin
+}
